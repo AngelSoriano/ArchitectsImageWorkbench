066921f6309fd4ccfe190a9f56dfc181fdf590f7
Scott D. Ligon <scottdligon@Scotts-MacBook-Air.local>
2017-02-14 18:18:18 -0800

    Implemented Sort Dropdown Button

--- architects-image-workbench/src/App.js
+++ architects-image-workbench/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './NavigationBar'
+import SortDropdownButton from './SortDropdownButton'
 
 class App extends Component {
     render() {
@@ -9,7 +10,7 @@ class App extends Component {
             <div className="App">
 
                 <NavigationBar/>
-
+		<SortDropdownButton/>
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h2>Welcome to React</h2>
---
+++ architects-image-workbench/src/SortDropdownButton.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {SplitButton,MenuItem} from 'react-bootstrap';
+
+class SortDropdownButton extends Component {
+
+    render() {
+        return (
+            <div className="SortDropdownButton">
+                <SplitButton title="Sort" pullRight id="split-button-pull-right">
+                    <MenuItem eventKey="1">Action</MenuItem>
+                    <MenuItem eventKey="2">Another action</MenuItem>
+                    <MenuItem eventKey="3">Something else here</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem eventKey="4">Separated link</MenuItem>
+                </SplitButton>
+            </div>
+        )
+    }
+
+}
+
+export default SortDropdownButton;
