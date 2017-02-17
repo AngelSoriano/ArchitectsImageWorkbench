1a447853b278925105d9d279bdfdfdf3a13c0099
Scott D. Ligon <scottdligon@Scotts-MacBook-Air.local>
2017-02-17 13:29:36 -0800

    Backup before pull

---
+++ architects-image-workbench/src/TestGallery.js
@@ -0,0 +1,11 @@
+import {Component} from 'react-images';
+import {Component} from 'react';
+
+class TestGallery extends React.Component{
+	imgs: [
+        "",
+    	],
+	render(){
+	   return (
+	      <div className="TestGallery>
+	      </div> 
