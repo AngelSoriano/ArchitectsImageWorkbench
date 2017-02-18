fc8fa8a523edadb47dc92a56cf84897f1b6a4981
ana <anaperez@csumb.edu>
2017-02-18 12:18:54 -0800

    Added page controller

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,26 +1,25 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './NavigationBar'
 import SortDropdownButton from './SortDropdownButton'
 import Gallery from './Gallery'
 import FilterButton from './FilterButton'
+import PageNavigation from './PageNavigation'
+import SearchBar from './SearchBar'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <NavigationBar/>
-                <SortDropdownButton/>
+                <SearchBar/>
                 <FilterButton/>
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
-		<Gallery/>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <PageNavigation/>
+		        <Gallery/>
+                <SortDropdownButton/>
+		        <Gallery/>
+                <SortDropdownButton/>
+		        <Gallery/>
             </div>
 
         );
