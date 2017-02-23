b7f71c1323fb2574dc31398f150a9d9b75f8f336
Naelin Aquino <nbcaquino@gmail.com>
2017-02-22 18:26:52 -0800

    Created retrieve image endpoint

--- react-client/src/service/ImageService.js
+++ react-client/src/service/ImageService.js
@@ -7,20 +7,28 @@
 
 import * as firebase from "firebase";
 import '../client'
-
 // Allow UUID generation
 const uuidV4 = require('uuid/v4');
 
-// Root reference to Firebase Storage instance (where images will be saved)
-var storageRef = firebase.storage().ref();
-
+/**
+ *  Uploads the image file to storage
+ *
+ *  @param String $file
+ *      The file path for the image we want to store
+ *
+ *  @todo
+ *      1. Better error-handling
+ *
+ */
 function upload(file) {
+    // Root reference to Firebase Storage instance (where images will be saved)
+    var storageRef = firebase.storage().ref();
+
     // Generate unique id for image
     const uniqueId = uuidV4()
 
     // Create a new reference for where newly uploaded image will be stored in Firebase Storage
-    // TODO: Allow users to enter a name/title for uploaded image
-    var imageRef = storageRef.child(uniqueId + "/temp-name");
+    var imageRef = storageRef.child("images/" + uniqueId);
 
     console.log(file)
     if (!file) {
@@ -28,12 +36,42 @@ function upload(file) {
     }
 
     // Store image into Firebase Storage
-    imageRef.put(file).then(function(snapshot) {
+    imageRef.put(file).then(function (snapshot) {
         console.log('Uploaded an image!');
+    });
+
+}
+
+/**
+ *  Retrieves the image URL according to the given image ID
+ *
+ *  @param String $imageId
+ *      The ID of the image URL we want to retrieve from storage
+ *
+ *  @todo
+ *      1. Better error-handling
+ *
+ */
+function retrieve(imageId) {
+    // Create a reference to Firebase Storage
+    var storageRef = firebase.storage().ref();
+    // Create a reference to the file we want to download
+    var imageRef = storageRef.child('images/' + imageId);
+
+    // Get the URL for the image we are retrieving
+    imageRef.getDownloadURL().then(function(url) {
+        // Once we have the download URL, then assign it to imageUrl
+        const imageUrl = url;
+        console.log(imageUrl.toString());
+        return imageUrl.toString();
+
+
+    }).catch(function(error) {
+        // If anything goes wrong while getting the download URL, log the error
+        console.error(error);
     });
 }
 
 
-
-const ImageService = { upload };
+const ImageService = {upload, retrieve};
 export default ImageService;
