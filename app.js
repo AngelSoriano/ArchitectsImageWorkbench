6a5d4b0ef784c3f4ccd408e4ac59166460d64209
expertReptile <asoriano@csumb.edu>
2017-03-06 11:09:50 -0800

    changes before pulling

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var images = require('./routes/images');
+var tags = require('./routes/retriveTags');
 var admin = require("firebase-admin");
 var app = express();
 
 // firebase-admin setup
-var serviceAccount = require("./service-account-key.json");
+//var serviceAccount = require("./service-account-key.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://architects-image-workbench.firebaseio.com/",
-    storageBucket: "gs://architects-image-workbench.appspot.com/"
-});
+//admin.initializeApp({
+  //  credential: admin.credential.cert(serviceAccount),
+    //databaseURL: "https://architects-image-workbench.firebaseio.com/",
+    //storageBucket: "gs://architects-image-workbench.appspot.com/"
+//});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/images', images);
+app.use('/retrieveTags', tags);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
