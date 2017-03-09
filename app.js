8784b67b7fb4d32df0ea73885e60d641875d1f12
Naelin Aquino <nbcaquino@gmail.com>
2017-03-09 11:46:00 -0800

    Save meta info, including tags, for each uploaded image to Firebase

--- app.js
+++ app.js
@@ -7,17 +7,10 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var images = require('./routes/images');
-var admin = require("firebase-admin");
 var app = express();
 
-// firebase-admin setup
-var serviceAccount = require("./service-account-key.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://architects-image-workbench.firebaseio.com/",
-    storageBucket: "gs://architects-image-workbench.appspot.com/"
-});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +20,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,21 +28,21 @@ app.use('/', index);
 app.use('/images', images);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
