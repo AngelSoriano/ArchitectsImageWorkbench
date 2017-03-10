f9278b6fa0f600a053707ccb8d71c64dc50de799
ana <anaperez@csumb.edu>
2017-03-10 12:55:19 -0800

    Writing uploadComponenet test

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -37,7 +37,5 @@ class App extends Component {
 
     );
     }
-
-
-    }
+}
 export default App;
---
+++ react-client/src/UploadComponent.test.js
@@ -0,0 +1,25 @@
+/**
+ * Created by anaperez on 3/8/17.
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import UploadedComponent from './UploadComponent';
+
+describe(UploadedComponent, () => {
+    const mockShowUploadModal = jest.fn();
+    const component = shallow(
+        <UploadedComponent showUploadModal = {mockShowUploadModal}/>
+    )
+    it('renders and matches our snapshot', () => {
+        const component = renderer.create(
+            <UploadedComponent/>
+        )
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    it('calls the passed in showUploadModal function when the upload button is clicked', () => {
+        component.find('button.Upload').simulate('click');
+        expect(mockShowUploadModal).toBeCalled();
+    });
+});
