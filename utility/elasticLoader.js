15502792d1316b83a6b7149e7a7af62749067cf2
expertReptile <asoriano@csumb.edu>
2017-03-10 13:38:54 -0800

    maybe this will work

--- utility/elasticLoader.js
+++ utility/elasticLoader.js
@@ -1,38 +1,43 @@
 /**
  * Created by kami on 3/8/17.
  */
-var Firebase = require('firebase');
+var admin = require("firebase-admin");
 var elastic = require('elasticsearch');
-
+console.log("Hello");
 // Firebase initialization
-firebase.intializeApp({
-    apiKey: '<your-api-key>',
-    authDomain: '<your-auth-domain>',
-    databaseURL: '<your-database-url>',
-    storageBucket: '<your-storage-bucket>'
+var serviceAccount = require("../service-account-key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://architects-image-workbench.firebaseio.com/",
+    storageBucket: "gs://architects-image-workbench.appspot.com/"
 });
 
 var config = {
     firebaseUrl:'https://architects-image-workbench.firebaseio.com',
     elasticSearchUrl: 'http://search-architect-images-6uxxalaigajwi7jnixorioabfa.us-west-1.es.amazonaws.com/'
 };
+console.log("firebase initialized");
 
-var rootRef = new Firebase(config.firebaseUrl);
+//var rootRef = new Firebase(config.firebaseUrl);
 
 var client = new elastic.Client({
     host: config.elasticSearchUrl
 });
 
-var labelRef = rootRef.child('Labels');
+console.log("elasticsearch configured");
+
+var labelRef = admin.child('Images');
 
 labelRef.on('child_added', upsert);
 labelRef.on('child_changed', upsert);
 labelRef.on('child_removed', remove);
 
 function upsert(snapshot) {
+    console.log("Upsert Stuck");
     client.index({
         index: 'firebase',
-        type: 'Labels',
+        type: 'Images',
         id: snapshot.key(),
         body: snapshot.val()
     }, function(err, response){
@@ -43,9 +48,10 @@ function upsert(snapshot) {
 }
 
 function remove(snapshot){
+    console.log("remove Stuck");
     client.delete({
         index: 'firebase',
-        type: 'Labels',
+        type: 'Images',
         id: snapshot.key()
     }, function(error, response){
         if(error){
