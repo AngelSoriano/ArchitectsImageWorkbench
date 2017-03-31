27b1f30d560747de20156e56980058b4e8e3a1a5
expertReptile <asoriano@csumb.edu>
2017-03-31 15:04:10 -0700

    flashlight is now posting queries into search/request. and we have a listener listening to reponses on search/response.

--- utility/flashlightSearchQueue.js
+++ utility/flashlightSearchQueue.js
@@ -4,25 +4,33 @@
 
 // This little node script listens at /search/request for incoming searches,
 // handles the interactions with ElasticSearch, and pushes results back into /search/response:
-var Firebase = require ('firebase');
+var firebase = require ('firebase-admin');
 var ElasticClient = require ('elasticsearchclient');//check with Angel
+conf = require('./flashlightConfig');
 
 // initialize our ElasticSearch API
 var client = new ElasticClient({ host: 'search-architect-images-6uxxalaigajwi7jnixorioabfa.us-west-1.es.amazonaws.com', port: 80 });
 
 // listen for requests at https://<INSTANCE>.firebaseio.com/search/request
-var queue = new Firebase('https://architects-image-workbench.firebaseio.com/search');
+var config = {
+  databaseURL: conf.FB_URL,
+  credential: firebase.credential.cert(conf.FB_SERVICEACCOUNT)
+};
+firebase = firebase.initializeApp(config, "queue");
+var database = firebase.database();
+var queue = database.ref('architects-image-workbench');
+//var queue = new Firebase('https://architects-image-workbench.firebaseio.com/search');
 queue.child('request').on('child_added', processRequest);
 
 function processRequest(snap) {
-    snap.ref().remove(); // clear the request after we receive it
-    var data = snap.val();
+    snap.ref.off(); // clear the request after we receive it
+    var dat = snap.val();
     // Query ElasticSearch
-    client.search(dat.index, dat.type, { "query": { 'match': { _id: dat.query } })
-        .on('data', function(data) {
+    client.search(dat.index, dat.type, { "query": { 'match': { _id: dat.query } }})
+      .on('data', function(data) {
             // Post the results to https://<INSTANCE>.firebaseio.com/search/response
-            queue.child('response/'+snap.key()).set(results);
-        })
-        .on('error', function(error){ /* process errors */ });
-.exec();
+            queue.child('response/'+snap.key).set(data);
+      })
+      .on('error', function(error){ /* process errors */ })
+      .exec()
 }
